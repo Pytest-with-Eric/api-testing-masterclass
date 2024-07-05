43b16bfd3756061a46e128b2f01b62fd8c36d346
ericsalesdeandrade <sdaeric19@gmail.com>
2024-07-05 12:42:34 +0100

    Use Env Vars

--- mortgage_calculator/app/database.py
+++ mortgage_calculator/app/database.py
@@ -1,8 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mortgage.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@localhost/mydatabase"
+load_dotenv()
+
+# Get Postgres Environment Variables
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./mortgage.db"
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
